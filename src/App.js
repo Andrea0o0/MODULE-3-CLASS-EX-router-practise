d8162a585ef954fb8f399dcbb5ebf1a374efe54d
Andrea0o0 <andreagarciagonzalez22@gmail.com>
2023-03-09 20:38:29 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom'
+import Contact from './views/Contact'
 import Home from './views/Home';
+import Projects from './views/Projects';
+import ErrorPage from './views/ErrorPage'
+import Navbar from './components/Navbar'
+import ProjectsApps from './components/ProjectsApps'
+import ProjectsGames from './components/ProjectsGames'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/projects" element={<Projects/>} >
+          <Route path="apps" element={<ProjectsApps/>} />
+          <Route path="games" element={<ProjectsGames />} />
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </div>
   );
 }
